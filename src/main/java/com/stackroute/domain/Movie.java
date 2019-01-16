c16ac5cccb3c1f10142108380370a76b15030622
meghanadiwakar <meghana567naidu@gmail.com>
2019-01-16 10:02:43 +0530

    task1

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -2,10 +2,10 @@ package com.stackroute.domain;
 
 public class Movie {
   private  Actor actor;
-    public Movie(){}
-    public Movie(Actor actor){
-        this.actor = actor;
-    }
+//    public Movie(){}
+//    public Movie(Actor actor){
+//        this.actor = actor;
+//    }
 
     public Actor getActor() {
         return actor;
