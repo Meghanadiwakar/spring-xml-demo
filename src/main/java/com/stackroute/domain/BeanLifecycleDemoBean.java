5c10dfd2248bfe3c122a8f52608000bd81664a6e
meghanadiwakar <meghana567naidu@gmail.com>
2019-01-16 09:45:16 +0530

    task4

---
+++ src/main/java/com/stackroute/domain/BeanLifecycleDemoBean.java
@@ -0,0 +1,16 @@
+package com.stackroute.domain;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class BeanLifecycleDemoBean  implements InitializingBean, DisposableBean {
+    public void destroy() throws Exception {
+        System.out.println("this is destroy method");
+
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("this is afterproperties set");
+
+    }
+}
